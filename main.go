dafe4177453e926259e4a5fe73e3a704da9e986d
Andreas Borglin <andreas@jakoon.co.uk>
2020-11-27 10:05:28 +0000

    Update main.go

--- main.go
+++ main.go
@@ -10,19 +10,19 @@ import (
 )
 
 const (
-	EnvSlackWebhook   = "SLACK_WEBHOOK"
-	EnvSlackIcon      = "SLACK_ICON"
-	EnvSlackIconEmoji = "SLACK_ICON_EMOJI"
-	EnvSlackChannel   = "SLACK_CHANNEL"
-	EnvSlackTitle     = "SLACK_TITLE"
-	EnvSlackMessage   = "SLACK_MESSAGE"
-	EnvSlackColor     = "SLACK_COLOR"
-	EnvSlackUserName  = "SLACK_USERNAME"
-	EnvSlackFooter    = "SLACK_FOOTER"
-	EnvGithubActor    = "GITHUB_ACTOR"
-	EnvSiteName       = "SITE_NAME"
-	EnvHostName       = "HOST_NAME"
-	EnvMinimal        = "MSG_MINIMAL"
+	EnvSlackWebhook  = "SLACK_WEBHOOK"
+	EnvSlackIcon     = "SLACK_ICON"
+	EnvSlackChannel  = "SLACK_CHANNEL"
+	EnvSlackTitle    = "SLACK_TITLE"
+	EnvSlackMessage  = "SLACK_MESSAGE"
+	EnvSlackColor    = "SLACK_COLOR"
+	EnvSlackUserName = "SLACK_USERNAME"
+	EnvGithubActor   = "GITHUB_ACTOR"
+	EnvChangeLogUrl  = "CHANGELOG_URL"
+	EnvReleasesUrl   = "RELEASES_URL"
+	EnvVariantsList  = "VARIANTS"
+	EnvSlackPretext  = "SLACK_PRETEXT"
+	EnvSlackFooter   = "SLACK_FOOTER"
 )
 
 type Webhook struct {
@@ -32,24 +32,33 @@ type Webhook struct {
 	IconEmoji   string       `json:"icon_emoji,omitempty"`
 	Channel     string       `json:"channel,omitempty"`
 	UnfurlLinks bool         `json:"unfurl_links"`
-	Attachments []Attachment `json:"attachments,omitempty"`
+	Attachments []Attachment `json:"attachments,omitmepty"`
 }
 
 type Attachment struct {
-	Fallback   string  `json:"fallback"`
-	Pretext    string  `json:"pretext,omitempty"`
-	Color      string  `json:"color,omitempty"`
-	AuthorName string  `json:"author_name,omitempty"`
-	AuthorLink string  `json:"author_link,omitempty"`
-	AuthorIcon string  `json:"author_icon,omitempty"`
-	Footer     string  `json:"footer,omitempty"`
-	Fields     []Field `json:"fields,omitempty"`
+	Text       string   `json:"text,omitempty"`
+	Title      string   `json:"title,omitempty"`
+	Fallback   string   `json:"fallback"`
+	Pretext    string   `json:"pretext,omitempty"`
+	Color      string   `json:"color,omitempty"`
+	AuthorName string   `json:"author_name,omitempty"`
+	AuthorLink string   `json:"author_link,omitempty"`
+	AuthorIcon string   `json:"author_icon,omitempty"`
+	Footer     string   `json:"footer,omitempty"`
+	Fields     []Field  `json:"fields,omitempty"`
+	Actions    []Action `json:"actions,omitempty"`
 }
 
 type Field struct {
 	Title string `json:"title,omitempty"`
 	Value string `json:"value,omitempty"`
 	Short bool   `json:"short,omitempty"`
+}
+
+type Action struct {
+	Type string `json:"type,omitempty"`
+	Text string `json:"text,omitempty"`
+	Url  string `json:"url,omitempty"`
 }
 
 func main() {
@@ -63,115 +72,60 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Message is required")
 		os.Exit(1)
 	}
-
-	minimal := os.Getenv(EnvMinimal)
-	fields := []Field{}
-	if minimal == "true" {
-		mainFields := []Field{
-			{
-				Title: os.Getenv(EnvSlackTitle),
-				Value: envOr(EnvSlackMessage, "EOM"),
-				Short: false,
-			},
-		}
-		fields = append(mainFields, fields...)
-	} else if minimal != "" {
-		requiredFields := strings.Split(minimal, ",")
-		mainFields := []Field{
-			{
-				Title: os.Getenv(EnvSlackTitle),
-				Value: envOr(EnvSlackMessage, "EOM"),
-				Short: false,
-			},
-		}
-		for _,requiredField := range requiredFields {
-			switch strings.ToLower(requiredField) {
-			case "ref":
-				field := []Field{
-					{
-						Title: "Ref",
-						Value: os.Getenv("GITHUB_REF"),
-						Short: true,
-					},
-				}
-				mainFields = append(field, mainFields...)
-			case "event":
-				field := []Field{
-					{
-						Title: "Event",
-						Value: os.Getenv("GITHUB_EVENT_NAME"),
-						Short: true,
-					},
-				}
-				mainFields = append(field, mainFields...)
-			case "actions url":
-				field := []Field{
-					{
-						Title: "Actions URL",
-						Value: "https://github.com/" + os.Getenv("GITHUB_REPOSITORY") + "/commit/" + os.Getenv("GITHUB_SHA") + "/checks",
-						Short: false,
-					},
-				}
-				mainFields = append(field, mainFields...)
-			}
-		}
-		fields = append(mainFields, fields...)
-	} else {
-		mainFields := []Field{
-			{
-				Title: "Ref",
-				Value: os.Getenv("GITHUB_REF"),
-				Short: true,
-			}, {
-				Title: "Event",
-				Value: os.Getenv("GITHUB_EVENT_NAME"),
-				Short: true,
-			},
-			{
-				Title: "Actions URL",
-				Value: "https://github.com/" + os.Getenv("GITHUB_REPOSITORY") + "/commit/" + os.Getenv("GITHUB_SHA") + "/checks",
-				Short: false,
-			},
-			{
-				Title: os.Getenv(EnvSlackTitle),
-				Value: envOr(EnvSlackMessage, "EOM"),
-				Short: false,
-			},
-		}
-		fields = append(mainFields, fields...)
+	ref := os.Getenv("GITHUB_REF")
+	refStart := 10
+	if strings.Contains(ref, "heads") {
+		refStart = 11	
 	}
+	refShort := ref[refStart:len(ref)]
 
-	hostName := os.Getenv(EnvHostName)
-	if hostName != "" {
-		newfields := []Field{
-			{
-				Title: os.Getenv("SITE_TITLE"),
-				Value: os.Getenv(EnvSiteName),
-				Short: true,
-			},
-			{
-				Title: os.Getenv("HOST_TITLE"),
-				Value: os.Getenv(EnvHostName),
-				Short: true,
-			},
-		}
-		fields = append(newfields, fields...)
+	fields:= []Field{
+		{
+			Title: "Version",
+			Value: os.Getenv(EnvSlackTitle),
+			Short: true,
+		},
+		{
+			Title: "Variants",
+			Value: os.Getenv(EnvVariantsList),
+			Short: true,
+		},
+		{
+			Title: "Built from",
+			Value: refShort,
+			Short: true,
+		},
+		{
+			Title: "Triggered by",
+			Value: envOr(EnvGithubActor, "N/A"),
+			Short: true,
+		},
+	}
+	actions:= []Action {
+		{
+			Type: "button",
+			Text: "Changelog",
+			Url: os.Getenv(EnvChangeLogUrl),
+		},
+		{
+			Type: "button",
+			Text: "Downloads",
+			Url: os.Getenv(EnvReleasesUrl),
+		},
 	}
 
 	msg := Webhook{
-		UserName:  os.Getenv(EnvSlackUserName),
-		IconURL:   os.Getenv(EnvSlackIcon),
-		IconEmoji: os.Getenv(EnvSlackIconEmoji),
-		Channel:   os.Getenv(EnvSlackChannel),
+		UserName: os.Getenv(EnvSlackUserName),
+		IconURL:  os.Getenv(EnvSlackIcon),
+		Channel:  os.Getenv(EnvSlackChannel),
 		Attachments: []Attachment{
 			{
-				Fallback:   envOr(EnvSlackMessage, "GITHUB_ACTION="+os.Getenv("GITHUB_ACTION")+" \n GITHUB_ACTOR="+os.Getenv("GITHUB_ACTOR")+" \n GITHUB_EVENT_NAME="+os.Getenv("GITHUB_EVENT_NAME")+" \n GITHUB_REF="+os.Getenv("GITHUB_REF")+" \n GITHUB_REPOSITORY="+os.Getenv("GITHUB_REPOSITORY")+" \n GITHUB_WORKFLOW="+os.Getenv("GITHUB_WORKFLOW")),
-				Color:      envOr(EnvSlackColor, "good"),
-				AuthorName: envOr(EnvGithubActor, ""),
-				AuthorLink: "http://github.com/" + os.Getenv(EnvGithubActor),
-				AuthorIcon: "http://github.com/" + os.Getenv(EnvGithubActor) + ".png?size=32",
-				Footer:     envOr(EnvSlackFooter, "<https://github.com/rtCamp/github-actions-library|Powered By rtCamp's GitHub Actions Library>"),
-				Fields:     fields,
+				Fallback: envOr(EnvSlackMessage, "GITHUB_ACTION=" + os.Getenv("GITHUB_ACTION") + " \n GITHUB_ACTOR=" + os.Getenv("GITHUB_ACTOR") + " \n GITHUB_EVENT_NAME=" + os.Getenv("GITHUB_EVENT_NAME") + " \n GITHUB_REF=" + os.Getenv("GITHUB_REF") + " \n GITHUB_REPOSITORY=" + os.Getenv("GITHUB_REPOSITORY") + " \n GITHUB_WORKFLOW=" + os.Getenv("GITHUB_WORKFLOW")),
+				Color:    envOr(EnvSlackColor, "good"),
+				Pretext:  envOr(EnvSlackPretext, ""),
+				Footer:   envOr(EnvSlackFooter, ""),
+				Fields:   fields,
+				Actions:  actions,
 			},
 		},
 	}
